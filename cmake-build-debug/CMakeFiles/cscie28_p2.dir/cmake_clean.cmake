ed747dd5942dc05c51ef6454ef850ad932a2978d
harjai <harjai@harjais-MacBook-Pro.local>
2018-02-17 18:11:59 -0800

    new clion project

---
+++ cmake-build-debug/CMakeFiles/cscie28_p2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cscie28_p2.dir/main.c.o"
+  "cscie28_p2.pdb"
+  "cscie28_p2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cscie28_p2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cscie28_p2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cscie28_p2.
+# This may be replaced when dependencies are built.
